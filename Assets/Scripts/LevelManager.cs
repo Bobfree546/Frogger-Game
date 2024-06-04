b0e648d22be4793a8390d1f6eee5a93379548f8d
Bobfree546 <amoshung9@gmail.com>
2024-06-04 18:43:43 +0800

    Add Automatic Row Spawning
    Update LevelManager
    Update LevelManager Prefab

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,53 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
     private static Vector3 PLAYER_SPAWN_LOCATION = new Vector3(0, 1, 0);
     private static Vector3 FIRST_ROW_SPAWN_LOCATION = new Vector3(0, 0, 0);
-    private const int ROWS_IN_ADVANCE = 20;
+    private const int ROWS_IN_ADVANCE = 50;
 
+    private int maxRowNum = 0;
+    private GameObject player;
 
     public GameObject playerPrefab;
     public GameObject withEnemyRowPrefab;
     public GameObject withoutEnemyRowPrefab;
     public Queue<GameObject> rowQueue;
-
+    
     // Start is called before the first frame update
     void Start()
     {
         rowQueue = new Queue<GameObject>();
         SpawnLevel();
+        
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (maxRowNum < (player.GetComponent<Player>().verticalCoordinate + ROWS_IN_ADVANCE))
+        {
+            SpawnRow();
+            maxRowNum++;
+        }
     }
 
-    void SpawnLevel()
+    private void SpawnLevel()
     {
-        Instantiate(playerPrefab, PLAYER_SPAWN_LOCATION, Quaternion.identity); //Spawn Player
+        player = Instantiate(playerPrefab, PLAYER_SPAWN_LOCATION, Quaternion.identity); //Spawn Player
 
-        GameObject previousRow = Instantiate(withoutEnemyRowPrefab, FIRST_ROW_SPAWN_LOCATION, Quaternion.identity); //Spawn First Row
-        rowQueue.Enqueue(previousRow);
+        GameObject firstRow = Instantiate(withoutEnemyRowPrefab, FIRST_ROW_SPAWN_LOCATION, Quaternion.identity); //Spawn First Row
+        rowQueue.Enqueue(firstRow);
 
         //Spawn next 19 rows
         for (int i = 0; i < ROWS_IN_ADVANCE; i++)
         {
-            previousRow = SpawnRow(previousRow);
-            rowQueue.Enqueue(previousRow);
+            SpawnRow();
         }
 
-
+        maxRowNum += (ROWS_IN_ADVANCE);
     }
 
-    private GameObject SpawnRow(GameObject previousRow)
+    private void SpawnRow()
     {
+        GameObject previousRow = rowQueue.Last();
         GameObject row = Instantiate(withEnemyRowPrefab, previousRow.transform.position + WithEnemyRowManager.ROW_SHIFT, Quaternion.identity);
-        return row;
+        rowQueue.Enqueue(row);
     }
 
 }
